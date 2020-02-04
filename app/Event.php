97766ca34a3803b6cf44fb647075660b958d79c9
Muhammad Saad <saad01.1999@gmail.com>
2020-02-04 22:38:38 +0500

    setup many to many CRUD relation for events and users

--- app/Event.php
+++ app/Event.php
@@ -6,5 +6,15 @@ use Illuminate\Database\Eloquent\Model;
 
 class Event extends Model
 {
-    //
+
+
+    /*
+    *--------------------------------------------------------------------------
+    * CRUD Relations
+    *--------------------------------------------------------------------------
+    */
+
+    public function users() {
+        return $this->belongsToMany('App\User');
+    }
 }
--- resources/views/layouts/navbars/sidebar.blade.php
+++ resources/views/layouts/navbars/sidebar.blade.php
@@ -2,9 +2,8 @@
 <ul class="navbar-nav bg-gradient-primary sidebar sidebar-dark accordion" id="accordionSidebar">
 
     <!-- Sidebar - Brand -->
-    <a class="sidebar-brand d-flex align-items-center justify-content-center" href="index.html">
+    <a class="sidebar-brand d-flex align-items-center justify-content-center" href="{{ route('home') }}">
         <div class="sidebar-brand-icon">
-            {{-- <i class="fas fa-laugh-wink"></i> --}}
             <i class="fas fa-code"></i>
         </div>
         <div class="sidebar-brand-text mx-3">{{ config('app.name', 'Laravel CMS') }}</div>
@@ -14,7 +13,7 @@
     <hr class="sidebar-divider my-0">
 
     <li class="nav-item active">
-      <a class="nav-link" href="{{ route('home') }}">
+      <a class="nav-link" href="{{ route('dashboard') }}">
         <i class="fas fa-fw fa-tachometer-alt"></i>
         <span>Dashboard</span></a>
     </li>
