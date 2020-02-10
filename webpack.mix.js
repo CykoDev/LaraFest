8ab3db6a7f6dda95ebc45a3462f67f2042e45082
Muhammad Saad <saad01.1999@gmail.com>
2020-02-10 22:16:32 +0500

    setup layouts and resources for public pages

--- webpack.mix.js
+++ webpack.mix.js
@@ -29,4 +29,17 @@ mix.styles([
     .scripts('resources/assets/sb-admin-2/js/demo/chart-area-demo.js', 'public/js/components/chart-area.js')
     .scripts('resources/assets/sb-admin-2/js/demo/chart-pie-demo.js', 'public/js/components/chart-pie.js')
     .scripts('resources/assets/sb-admin-2/js/demo/chart-bar-demo.js', 'public/js/components/chart-bar.js')
-    .scripts('resources/assets/sb-admin-2/js/demo/datatables-demo.js', 'public/js/components/datatables.js');
+    .scripts('resources/assets/sb-admin-2/js/demo/datatables-demo.js', 'public/js/components/datatables.js')
+    .styles([
+        'resources/assets/vendor/fontawesome/css/all.min.css',
+        'resources/assets/sb-admin-2/css/sb-admin-2.min.css',
+        'resources/assets/vendor/bootstrap/css/bootstrap.min.css',
+        'resources/assets/grayscale/css/grayscale.min.css',
+    ], 'public/css/public.css')
+    .scripts([
+        'resources/assets/vendor/jquery/jquery.min.js',
+        'resources/assets/vendor/jquery-easing/jquery.easing.min.js',
+        'resources/assets/vendor/bootstrap/js/bootstrap.bundle.min.js',
+        'resources/assets/vendor/fontawesome/js/all.min.js',
+        'resources/assets/grayscale/js/grayscale.min.js',
+    ], 'public/js/public.js');
