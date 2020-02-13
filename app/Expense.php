5cb39ab89731130fcb6b96758106cf2c0cdb194f
Anas Tasadduq <anas.tasadduq@gmail.com>
2020-02-14 00:10:15 +0500

    make expenses work for packages

--- app/Expense.php
+++ app/Expense.php
@@ -6,5 +6,11 @@ use Illuminate\Database\Eloquent\Model;
 
 class Expense extends Model
 {
-    //
+    protected $fillable = [
+        'expendable_id', 'expendable_type', 'price', 'user_id',
+    ];
+
+    public function expendable() {
+        return $this->morphTo();
+    }
 }
