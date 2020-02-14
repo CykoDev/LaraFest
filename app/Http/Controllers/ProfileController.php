b62a1fb834cd3f0edc8bd1ddc25e2860a710c516
Muhammad Saad <saad01.1999@gmail.com>
2020-02-14 06:49:06 +0500

    create base structure for password updation

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use App\http\Requests\ApplicantProfileRequest;
+use App\http\Requests\ProfileUpdateRequest;
 
 use App\User;
 use App\Photo;
--- routes/web.php
+++ routes/web.php
@@ -4,29 +4,18 @@
 |--------------------------------------------------------------------------
 | Web Routes
 |--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
 */
 
 
 
 Auth::routes(['verify' => true]);
 
-Route::get('/', function(){
-
-    // return view('welcome');
+Route::get('/', function () {
     return view('public.page-one');
 })->name('home');
 
-Route::get('/dashboard', ['as'=>'dashboard', 'uses'=>'HomeController@index']);
+Route::get('/dashboard', ['as' => 'dashboard', 'uses' => 'HomeController@index']);
 
-
-
-Route::get('/media/delete/{filepath}', ['as'=>'delete', 'uses'=>'MediaController@delete']);
-Route::get('/media/download/{filepath}', ['as'=>'download', 'uses'=>'MediaController@download']);
 
 /*
 |--------------------------------------------------------------------------
@@ -37,30 +26,42 @@ Route::get('/media/download/{filepath}', ['as'=>'download', 'uses'=>'MediaContro
 // Route::get('/page-one', function() { return view('public.page-one'); });
 
 
-
 /*
 |--------------------------------------------------------------------------
 | Portal Routes
 |--------------------------------------------------------------------------
 */
 
-Route::group(['middleware'=>'verified'], function(){
+Route::group(['middleware' => 'verified'], function () {
 
     Route::resource('users', 'UserController');
-    Route::get('/users-role/{role}', ['as'=>'users.index-role', 'uses'=>'UserController@indexRole']);
+    Route::get('/users-role/{role}', ['as' => 'users.index-role', 'uses' => 'UserController@indexRole']);
 
     Route::resource('roles', 'RoleController');
 
     Route::resource('events', 'EventController');
-
     Route::resource('event/types', 'EventTypeController');
-
-    // Route::resource('profile', 'ProfileController');
 
     Route::resource('packages', 'PackageController');
 
     Route::resource('media', 'MediaController');
-    Route::post('media/bulk-manage', ['as'=>'media.manageMany', 'uses'=>'MediaController@manageMany']);
+    Route::post('media/bulk-manage', ['as' => 'media.manageMany', 'uses' => 'MediaController@manageMany']);
+    Route::get('/media/delete/{filepath}', ['as' => 'delete', 'uses' => 'MediaController@delete']);
+    Route::get('/media/download/{filepath}', ['as' => 'download', 'uses' => 'MediaController@download']);
+
+
+    /*
+    |--------------------------------------------------------------------------
+    | Profile Routes
+    |--------------------------------------------------------------------------
+    */
+
+    Route::get('profile', ['as' => 'profile.show', 'uses' => 'ProfileController@show']);
+    Route::post('profile', ['as' => 'profile.store', 'uses' => 'ProfileController@store']);
+    Route::get('profile/edit', ['as' => 'profile.edit', 'uses' => 'ProfileController@edit']);
+    Route::patch('profile/edit/{route}', ['as' => 'profile.update', 'uses' => 'ProfileController@update']);
+    Route::get('applicant/profile', ['as' => 'profile.applicant.edit', 'uses' => 'ProfileController@editApplicant']);
+    Route::patch('applicant/profile/{route}', ['as' => 'profile.applicant.update', 'uses' => 'ProfileController@updateApplicant']);
 
 
     /*
@@ -77,27 +78,10 @@ Route::group(['middleware'=>'verified'], function(){
     Route::post('export/events', 'ExportController@exportEvents');
 });
 
-Route::get('profile', ['as'=>'profile.show', 'uses'=>'ProfileController@show']);
-Route::post('profile', ['as'=>'profile.store', 'uses'=>'ProfileController@store']);
-Route::get('profile/edit', ['as'=>'profile.edit', 'uses'=>'ProfileController@edit']);
-Route::get('applicant/profile/setup', ['as'=>'profile.applicant.edit', 'uses'=>'ProfileController@editApplicant']);
-Route::patch('profile/update/{route}', ['as'=>'profile.update', 'uses'=>'ProfileController@update']);
-Route::patch('applicant/profile/update/{route}', ['as'=>'profile.applicant.update', 'uses'=>'ProfileController@updateApplicant']);
+
 
 /*
 |--------------------------------------------------------------------------
 | Test Routes
 |--------------------------------------------------------------------------
 */
-
-Route::get('pdf/download', 'InvoiceController@generatepdf');
-
-Route::Get('pdf', function(){
-
-    return view('pdf.invoice');
-});
-
-
-
-
-
