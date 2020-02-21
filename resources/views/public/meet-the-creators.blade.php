09974463b3b17ffc862fe5c3f97705ce457c7509
Muhammad Saad <saad01.1999@gmail.com>
2020-02-21 19:56:50 +0500

    redesign meet-the-devs page

--- resources/views/public/meet-the-creators.blade.php
+++ resources/views/public/meet-the-creators.blade.php
@@ -90,6 +90,91 @@
         </div>
     </div>
 
+    <div class="card shadow mb-4 mx-4">
+        <div class="card-header py-3">
+            <h6 class="m-0 font-weight-bold text-primary">Meet The Dev House</h6>
+            <small>Um yea</small>
+        </div>
+        <div class="card-body">
+            <div class="text-center mt-2">
+                <a class="btn btn-default" target="_blank" href="https://github.com/CykoDev">
+                    <h1 class="display-3">CykoDev</h1>
+                </a>
+            </div>
+            <div class="text-center mb-3">
+                <a href="https://github.com/aitasadduq" class="btn btn-default px-1" target="_blank">
+                    <img height="40" heignt="40" src="{{ asset('img/public/creator_anas.jpg') }}" class="rounded-circle">
+                </a>
+                <a href="https://github.com/msaad1999" class="btn btn-default px-1" target="_blank">
+                    <img height="40" heignt="40" src="{{ asset('img/public/creator_saad.jpg') }}" class="rounded-circle">
+                </a>
+                <a href="https://github.com/skamal16" class="btn btn-default px-1" target="_blank">
+                    <img height="40" heignt="40" src="{{ asset('img/public/creator_kamal.jpg') }}" class="rounded-circle">
+                </a>
+            </div>
+            <div class="text-center">
+                <p class="font-weight-bold text-primary">
+                    "Braccas meas vescimini"
+                </p>
+                <a class="btn btn-default py-0" href="mailto:cykodev@gmail.com">
+                    <small class="text-muted">
+                        cykodev@gmail.com
+                    </small>
+                </a>
+            </div>
+            <div class="text-center mt-4">
+                <div class="row pt-3">
+                    <div class="col ml-5 mr-2">
+                        <div class="card shadow mb-4">
+                            <div class="card-body">
+                                <div class="row">
+                                    <div class="col-4 my-auto">
+                                        <a href="https://github.com/CykoDev/GitKLiK" class="btn btn-default px-1" target="_blank">
+                                            <i class="fas fa-4x fa-code-branch text-warning"></i>
+                                        </a>
+                                    </div>
+                                    <div class="col-8 text-left">
+                                        <a href="https://github.com/CykoDev/GitKLiK" class="btn btn-default px-1" target="_blank">
+                                            <h3 class="text-dark">
+                                                GitKLiK
+                                            </h3>
+                                        </a>
+                                        <p class="small">
+                                            Laravel Version Control Application employing Git, with remote repositories. Made with GitHub as inspiration
+                                        </p>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col mr-5 ml-2">
+                        <div class="card shadow mb-4">
+                            <div class="card-body">
+                                <div class="row">
+                                    <div class="col-4 my-auto">
+                                        <a href="https://github.com/CykoDev/LaraFest" class="btn btn-default px-1" target="_blank">
+                                            <i class="fab fa-4x fa-laravel text-danger"></i>
+                                        </a>
+                                    </div>
+                                    <div class="col-8 text-left">
+                                        <a href="https://github.com/CykoDev/LaraFest" class="btn btn-default px-1" target="_blank">
+                                            <h3 class="text-dark">
+                                                LaraFest
+                                            </h3>
+                                        </a>
+                                        <p class="small">
+                                            Laravel Festival management application with roles, data exports, multiple events, challan generation and more.
+                                        </p>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
 </div>
 
 @endsection
